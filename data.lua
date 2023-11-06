e9348f4c543cfaf0eed363c370e364f4ddde6ef7
DerPumu <derpumu@users.noreply.github.com>
2023-11-06 17:11:55 +0100

    refactor to generic loop

--- data.lua
+++ data.lua
@@ -14,18 +14,28 @@ if settings.startup["colloquial-transport-belt"].value == "colour" then
     end
 end
 
+local replacements = {
+    circuits = {
+        type = "item",
+        names = {"electronic-circuit", "advanced-circuit", "processing-unit"}
+    },
+    modules = {
+        type = "module",
+        names = {
+            "speed-module", "speed-module-2", "speed-module-3",
+            "effectivity-module", "effectivity-module-2", "effectivity-module-3",
+            "productivity-module", "productivity-module-2", "productivity-module-3",
+        }
+    },
+}
 
-if settings.startup["colloquial-circuits"].value ~= "vanilla" then
-    for _, name in pairs({"electronic-circuit", "advanced-circuit", "processing-unit"}) do
-        data.raw.item[name].localised_name = {"colloquial."..name.."-".. settings.startup["colloquial-circuits"].value}
-    end
-end
-
-
-if settings.startup["colloquial-modules"].value ~= "vanilla" then
-    for _, name in pairs({ "speed-module", "speed-module-2", "speed-module-3", "effectivity-module", "effectivity-module-2", "effectivity-module-3", "productivity-module", "productivity-module-2", "productivity-module-3",
-    }) do
-        assert(data.raw.module[name], "module " .. name)
-        data.raw.module[name].localised_name = {"colloquial."..name.."-".. settings.startup["colloquial-modules"].value}
+for setting, definition in pairs(replacements) do
+    local setting_value = settings.startup["colloquial-"..setting].value
+    if setting_value ~= "vanilla" then
+        local type = definition.type
+        for _, name in pairs(definition.names) do
+            assert(data.raw[type][name], type .. " " .. name .. " does not exist in data.raw")
+            data.raw[type][name].localised_name = {"colloquial."..name.."-".. setting_value}
+        end
     end
 end
