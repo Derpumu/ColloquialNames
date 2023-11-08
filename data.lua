e614c226682bc0fc7181c8969a7c04fc6d47a527
DerPumu <derpumu@users.noreply.github.com>
2023-11-08 09:48:21 +0100

    fix loader key not found

--- data.lua
+++ data.lua
@@ -1,27 +1,15 @@
-
-if settings.startup["colloquial-transport-belt"].value == "colour" then
-    local colours = {
-        [""] = "yellow",
-        fast = "red",
-        express = "blue"
-    }
-
-    for _, type in pairs({"transport-belt", "underground-belt", "splitter", "loader"}) do
-        for k, colour in pairs(colours) do
-            name = (k == "") and type or k .. "-" .. type
-            data.raw[type][name].localised_name = {"colloquial."..type.."-colour", {"colloquial." .. colour}}
-        end
-    end
-end
-
 local replacements = {
+    ["transport-belt"] = {
+        ["transport-belt"] = { "transport-belt", "fast-transport-belt", "express-transport-belt" },
+        ["splitter"] = { "splitter", "fast-splitter", "express-splitter" },
+        ["underground-belt"] = { "underground-belt", "fast-underground-belt", "express-underground-belt" },
+        ["loader"] = { "loader", "fast-loader", "express-loader" },
+    },
     circuits = {
-        type = "item",
-        names = {"electronic-circuit", "advanced-circuit", "processing-unit"}
+        item = { "electronic-circuit", "advanced-circuit", "processing-unit" }
     },
     modules = {
-        type = "module",
-        names = {
+        module = {
             "speed-module", "speed-module-2", "speed-module-3",
             "effectivity-module", "effectivity-module-2", "effectivity-module-3",
             "productivity-module", "productivity-module-2", "productivity-module-3",
@@ -30,12 +18,14 @@ local replacements = {
 }
 
 for setting, definition in pairs(replacements) do
-    local setting_value = settings.startup["colloquial-"..setting].value
+    local setting_value = settings.startup["colloquial-" .. setting].value
     if setting_value ~= "vanilla" then
-        local type = definition.type
-        for _, name in pairs(definition.names) do
-            assert(data.raw[type][name], type .. " " .. name .. " does not exist in data.raw")
-            data.raw[type][name].localised_name = {"colloquial."..name.."-".. setting_value}
+        for type, names in pairs(definition) do
+            for _, name in pairs(names) do
+                if data.raw[type] and data.raw[type][name] then
+                    data.raw[type][name].localised_name = { "colloquial." .. name .. "-" .. setting_value }
+                end
+            end
         end
     end
 end
